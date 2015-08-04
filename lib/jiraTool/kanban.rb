4447cc05de3a2fbf202379313612952f16825314
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2015-08-04 10:44:29 -0500

    Add an ascii kanban table

---
+++ lib/jiraTool/kanban.rb
@@ -0,0 +1,48 @@
+require 'terminal-table'
+require 'pp'
+
+command :kanban do |c|
+  c.syntax = 'jira query [options] kanban'
+  c.summary = 'Show a kanban table'
+  c.description = ''
+  c.example 'description', 'command example'
+	c.option '-u', '--[no-]assignedSelf', 'Prefix query with assignee test'
+	c.option '-p', '--[no-]project', 'Prefix query with project test'
+	c.option '-w', '--width WIDTH', 'Width of the table'
+  c.action do |args, options|
+		options.default :project=>true,
+			:assignedSelf=>true,
+			:width=>HighLine::SystemExtensions.terminal_size[0]
+
+		cW = (options.width.to_i - 4) / 3
+
+		jira = JiraUtils.new(args, options)
+		qBase = []
+		qBase.unshift("assignee = #{jira.username} AND") if options.assignedSelf
+		qBase.unshift("project = #{$cfg['.jira.project']} AND") if options.project
+
+		## things to do
+		q = qBase + [%{(status = open OR status = "On Deck" OR status = "Waiting Estimation Approval" OR status = "Testing - Bug Found")}]
+		todo = jira.getIssues(q.join(' ')).map{|i| "#{i['key']}\n #{i['fields']['summary'][0..cW]}"}
+
+		## Things working on
+		q = qBase + [%{status = "In Progress"}]
+		inP = jira.getIssues(q.join(' ')).map{|i| "#{i['key']}\n #{i['fields']['summary'][0..cW]}"}
+
+		## Things in testing
+		q = qBase + [%{status = Testing}]
+		test = jira.getIssues(q.join(' ')).map{|i| "#{i['key']}\n #{i['fields']['summary'][0..cW]}"}
+
+		## pad out short
+		longest = [todo.length, inP.length, test.length].max
+		todo.fill(' ', todo.length .. longest) if todo.length <= longest
+		inP.fill(' ', inP.length .. longest) if inP.length <= longest
+		test.fill(' ', test.length .. longest) if test.length <= longest
+
+		rows = [todo, inP, test].transpose
+		table = Terminal::Table.new :headings => ["TODO", "In Progress", "Testing"], :rows=>rows
+		puts table
+
+  end
+end
+#  vim: set sw=2 ts=2 :
