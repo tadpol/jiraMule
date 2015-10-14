f4598430e5a6e56290129440e90a556840856325
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2015-10-14 08:44:45 -0500

    Moved Query to its own file

---
+++ lib/jiraMule/query.rb
@@ -0,0 +1,29 @@
+
+command :query do |c|
+  c.syntax = 'jira query [options] query'
+  c.summary = 'Get the keys from a jira query'
+  c.description = 'Run a query. '
+	c.example 'Get Open issues and dump everything', %{jira query status=Open --fields "" --json}
+	c.option '--[no-]raw', 'Do not prefix query with project and assignee'
+	c.option '--[no-]json', 'Output json reply instead of summary'
+	c.option '--fields FIELDS', Array, ''
+  c.action do |args, options|
+		options.defaults :json => false
+		jira = JiraUtils.new(args, options)
+		args.unshift("assignee = #{jira.username} AND") unless options.raw
+		args.unshift("project = #{jira.project} AND") unless options.raw
+		q = args.join(' ')
+		if options.fields then
+			issues = jira.getIssues(q, options.fields)
+		else
+			issues = jira.getIssues(q)
+		end
+		if options.json then
+			puts JSON.dump(issues)
+		else
+			keys = issues.map {|item| item['key'] + ' ' + item.access('fields.summary')}
+			keys.each {|k| puts k}
+		end
+  end
+end
+#  vim: set sw=2 ts=2 :
