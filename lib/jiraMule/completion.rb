ea5a3a63eb01a812b1e9455026523acdbe0b1ab1
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2016-06-03 10:13:15 -0500

    get global and subcmd options

--- lib/jiraMule/completion.rb
+++ lib/jiraMule/completion.rb
@@ -1,28 +1,38 @@
 require 'pp'
 
 command :completion do |c|
-  c.syntax = %{jm completion <> }
+  c.syntax = %{jm completion [options] }
   c.summary = %{Tool for getting bits for tab completion.
 
   For starts, this is zsh only. Because that is what I use.
 }
   c.option '--subs', 'List sub commands'
-  c.option '--opts', ''
+  c.option '--opts CMD', 'List options for subcommand'
+  c.option '--gopts', 'List global options'
 
-  # - Get sub commands.
-  # - get options for a command
   # - dump a completion file that can be loaded into shell
+
+  # it feels like I could make a HelpFormatter that actually dumps in a completion
+  # script format.
 
   c.action do |args, options|
 
     runner = ::Commander::Runner.instance
+    #pp runner
+    if options.gopts then
+      opts = runner.instance_variable_get(:@options)
+      opts.each do |o|
+        say "{#{o[:switches].join(',')}}::#{o[:description]}"
+      end
+    end
+
     if options.subs then
       say runner.instance_variable_get(:@commands).keys.join(' ')
     end
 
     if options.opts then
       cmds = runner.instance_variable_get(:@commands)
-      cmds[args[0]].options.each do |o|
+      cmds[options.opts].options.each do |o|
         say "{#{o[:switches].join(',')}}::#{o[:description]}"
       end
     end
